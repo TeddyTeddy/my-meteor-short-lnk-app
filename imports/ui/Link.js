2b45efe0261d575f371312594955d7cf569b51f6
HC <hakan.cuzdan@yandex.com>
2018-03-31 19:22:21 +0300

    Section 6, Lecture55 : Adding a BUTTON to Links page which takes to Login Page Part 2

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -5,9 +5,9 @@ export default class Link extends React.Component {
   render() {
     return (
       <div>
+        <h1>Your Links</h1>
         <button onClick={() => browserHistory.push('/')}>Logout</button>
       </div>
     );
-
   }
 }
