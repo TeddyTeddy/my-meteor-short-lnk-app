e1876e9c6c765e4f60d500b431f289c6120f2edf
HC <hakan.cuzdan@yandex.com>
2018-03-31 17:10:28 +0300

    Section 6, Lecture54 : Signup and Link components in Router react component

--- client/main.js
+++ client/main.js
@@ -3,7 +3,16 @@ import Signup from '../imports/ui/Signup';
 import Link from '../imports/ui/Link';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { Router, Route, browserHistory } from 'react-router';
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/signup" component={Signup}/>
+    <Route path="/links" component={Link}/>
+  </Router>
+);
+
 
 Meteor.startup(() => {
-  ReactDOM.render(<Link/>, document.getElementById('app'));
+  ReactDOM.render(routes, document.getElementById('app'));
 });
