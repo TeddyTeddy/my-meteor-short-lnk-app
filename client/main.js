f65a8da84ddf8dbebda77ffb4b640bfca43a15a2
HC <hakan.cuzdan@yandex.com>
2018-03-31 17:17:53 +0300

    Section 6, Lecture54 : NotFound component added along with the route

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import Signup from '../imports/ui/Signup';
 import Link from '../imports/ui/Link';
+import NotFound from '../imports/ui/NotFound';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Router, Route, browserHistory } from 'react-router';
@@ -9,6 +10,7 @@ const routes = (
   <Router history={browserHistory}>
     <Route path="/signup" component={Signup}/>
     <Route path="/links" component={Link}/>
+    <Route path="*" component={NotFound}/>
   </Router>
 );
 
