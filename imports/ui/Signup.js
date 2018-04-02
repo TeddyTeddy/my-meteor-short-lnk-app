fff4560d07d5b25cb63bb7c29c6459f7620a5c30
HC <hakan.cuzdan@yandex.com>
2018-04-02 14:39:28 +0300

    Section 6, Lecture 56: Adding a count state to Signup component and incrementing and decrementing those

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,11 +2,33 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment() {
+      this.setState({
+        count: this.state.count + 1
+      });
+  }
+
+  decrement(){
+    this.setState({
+        count: this.state.count - 1
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Signup to Short Lnk</h1>
         <p>Sign up form here</p>
+        <p>{this.state.count}</p>
+        <button onClick={this.increment.bind(this)}>+1</button>
+        <button onClick={this.decrement.bind(this)}>-1</button>
         <Link to='/'>Already have an account? Login here</Link>
       </div>
     );
