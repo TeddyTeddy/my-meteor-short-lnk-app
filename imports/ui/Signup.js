b56a8ecaf3ea98dc58f7c6c02610976adcab23f3
HC <hakan.cuzdan@yandex.com>
2018-04-15 14:42:25 +0300

    Section 6, Lecture 57 : SignupForm connected to Accounts.createUser()

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { Accounts } from 'meteor/accounts-base';
 
 export default class Signup extends React.Component {
   constructor(props) {
@@ -11,9 +12,14 @@ export default class Signup extends React.Component {
 
   onSubmit(e) {
       e.preventDefault();
-      this.setState({
-        text: 'Something went wrong'
+      let email = this.refs.email.value.trim();
+      let password = this.refs.password.value.trim();
+      Accounts.createUser({email, password}, (err) => {
+        console.log('Signup callback:', err);
       });
+      // this.setState({
+      //  text: 'Something went wrong'
+      // });
   }
 
   render() {
@@ -24,8 +30,8 @@ export default class Signup extends React.Component {
 
         {this.state.text ? <p>{this.state.text}</p> : undefined}
         <form>
-            <input type='email' name='email' placeholder='Email'></input>
-            <input type='password' name='password' placeholder='Password'></input>
+            <input type='email' ref='email' name='email' placeholder='Email'></input>
+            <input type='password' ref='password' name='password' placeholder='Password'></input>
             <button onClick={this.onSubmit.bind(this)}>Signup</button>
         </form>
         <Link to='/'>Already have an account? Login here</Link>
