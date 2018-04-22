146ff8f024293b98a24296d5e11f3e7869b4a5bc
HC <hakan.cuzdan@yandex.com>
2018-04-22 13:22:13 +0300

    Section 6, Lecture 59: Authentication redirecting to the right page : Challenge Completed

--- client/main.js
+++ client/main.js
@@ -6,8 +6,10 @@ import Login from '../imports/ui/Login';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Router, Route, browserHistory } from 'react-router';
+import {Tracker} from 'meteor/tracker';
 
-
+const unauthenticatedPages = ['/', '/signup'];
+const authenticatedPages = ['/links'];
 const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={Login}/>
@@ -17,6 +19,26 @@ const routes = (
   </Router>
 );
 
+Tracker.autorun(()=> {
+  const isAuthenticated = !!Meteor.userId();
+  const pathname =  browserHistory.getCurrentLocation().pathname;
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+  const isAutenticatedPage = authenticatedPages.includes(pathname);
+
+  // if on unauthenticated page and logged in, redirect to /links page via
+  // browserHistory.push()
+  if(isUnauthenticatedPage && isAuthenticated) {
+    browserHistory.push('/links');
+  }
+
+  // if on an authenticated page, and not logged in, redirect to '/' page via
+  // browserHistory.push()
+  if(isAutenticatedPage && !isAuthenticated) {
+    browserHistory.push('/');
+  }
+  // No else
+
+});
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
