0312ea7f4b1a76d8dc28119877a8e643622ebb10
HC <hakan.cuzdan@yandex.com>
2018-04-28 14:49:47 +0300

    Section 6, Lecture 62: employeeSchema challange done

--- server/main.js
+++ server/main.js
@@ -3,27 +3,53 @@ import SimpleSchema from 'simpl-schema';
 
 Meteor.startup(() => {
   // code to run on server at startup
-const petSchema = new SimpleSchema({
-  name: {
-    type: String,
-    min: 1,
-    max: 200,
-    optional: true
-  },
-  age: {
-    type: Number,
-    min: 0
-  },
-  contactNumber: {
-    type: String,
-    optional: true,
-    regEx: SimpleSchema.RegEx.Phone
-  }
+// const petSchema = new SimpleSchema({
+//   name: {
+//     type: String,
+//     min: 1,
+//     max: 200,
+//     optional: true
+//   },
+//   age: {
+//     type: Number,
+//     min: 0
+//   },
+//   contactNumber: {
+//     type: String,
+//     optional: true,
+//     regEx: SimpleSchema.RegEx.Phone
+//   }
+// });
+//
+// petSchema.validate({
+//   age: 21,
+//   contactNumber: 'abcd233#'
+// });
+
+// employeeSchema
+// name - a string between 1 to 200 chars
+// hourlyWage - a number greater than zero
+// email: should be e-mail format
+const employeeSchema = new SimpleSchema({
+   name: {
+     type: String,
+     min: 1,
+     max: 200
+   },
+   hourlyWage: {
+     type: Number,
+     min: 0
+   },
+   email: {
+     type: String,
+     regEx: SimpleSchema.RegEx.Email
+   }
 });
 
-petSchema.validate({
-  age: 21,
-  contactNumber: 'abcd233#'
+employeeSchema.validate({
+  name: 'Hakan',
+  hourlyWage: 200,
+  email: 'hakan@gmail.com'
 });
 
 });
