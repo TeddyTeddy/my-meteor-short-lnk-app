02d0a8c459983f9f025c741e9b3c7a89436e0f31
HC <hakan.cuzdan@yandex.com>
2018-04-28 17:47:37 +0300

    Section 6, Lecture 63 : Accounts.validateNewUser() on server side

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -28,7 +28,7 @@ export default class Login extends React.Component {
       <div>
         <h1>Login to Short Lnk</h1>
         {this.state.error ? <p>{this.state.error}</p> : undefined}
-        <form onSubmit={this.onSubmit.bind(this)}>
+        <form onSubmit={this.onSubmit.bind(this)} noValidate>
             <input type='email' ref='email' name='email' placeholder='Email'></input>
             <input type='password' ref='password' name='password' placeholder='Password'></input>
             <button>Login</button>
--- server/main.js
+++ server/main.js
@@ -1,5 +1,25 @@
 import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
+import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Accounts.validateNewUser((user)=>{
+    // console.log('this is the user', user);
+    // I20180428-17:31:27.112(3)? this is the user { createdAt: 2018-04-28T14:31:27.108Z,
+    // I20180428-17:31:27.112(3)?   _id: 'm9bffxqur2P79hK6B',
+    // I20180428-17:31:27.113(3)?   services:
+    // I20180428-17:31:27.113(3)?    { password:
+    // I20180428-17:31:27.113(3)?       { bcrypt: '$2a$10$zjPnHTHfp./VIzlaKkz7Bulk2pJLmxMXTX31UCukNCqzRPVLLUc4e' } },
+    // I20180428-17:31:27.114(3)?   emails: [ { address: 'hakan@example.com', verified: false } ] }
+    const email = user.emails[0].address;
+    new SimpleSchema({
+      email: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Email
+      }
+    }).validate(email);
+
+    return true;
+  });
 });
